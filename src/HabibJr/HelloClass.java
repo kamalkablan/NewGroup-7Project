81f87fd31a7a5697fee03ed790c21087e5edfe80
habib <74197575+garrarihabib@users.noreply.github.com>
2020-11-17 18:44:21 -0500

    HabibJrClass

--- src/HabibJr/HelloClass.java
+++ src/HabibJr/HelloClass.java
@@ -3,5 +3,6 @@ package HabibJr;
 public class HelloClass {
     public static void main(String[] args) {
         System.out.println("Hello Class");
+        System.out.println("How are you");
     }
 }
