a67a725dfc2b477cf1dcf285016728c3ca8382da
Habib Azarsina <azarsinah@gmail.com>
2020-11-18 09:42:57 -0500

    new project Fist try to push a file.

--- src/Azarsina/Task001.java
+++ src/Azarsina/Task001.java
@@ -1,4 +1,12 @@
 package Azarsina;
 
 public class Task001 {
+
+
+    public static void main(String[] args) {
+       // String str
+        System.out.println("Hi. This is a test by Habib.");
+    }
 }
+
+
